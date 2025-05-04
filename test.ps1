33d90fea1681426d64f3ca5ebfa94589a107b27a
DSD-tobyyeager <toby.yeager@doverschools.net>
2025-05-04 10:38:11 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,116 +1,103 @@
-#================================================
-#   OSDCloud Task Sequence
-#   Windows 10 21H1 Pro en-us Retail
-#   No Autopilot
-#   No Office Deployment Tool
-#================================================
-#   PreOS
-#   Install and Import OSD Module
-#================================================
-Install-Module OSD -Force
-Import-Module OSD -Force
-#================================================
-#   [OS] Start-OSDCloud with Params
-#================================================
-$Params = @{
-    OSBuild = "24H2"
-    OSEdition = "Pro"
-    OSLanguage = "en-us"
-    OSLicense = "Volume"
-    SkipAutopilot = $true
-    SkipODT = $true
+<#
+.SYNOPSIS
+    AutopilotOOBE PSCloudScript at autopilotoobe.ps1.osdeploy.com
+.DESCRIPTION
+    AutopilotOOBE PSCloudScript at autopilotoobe.ps1.osdeploy.com
+.NOTES
+    Requires Windows 10 1809+ - Windows 11
+    In OOBE (Out-of-Box Experience), press Shift + F10 to open a Command Prompt
+.LINK
+    https://github.com/OSDeploy/ps1.osdeploy.com/blob/main/autopilotoobe.ps1
+.EXAMPLE
+    powershell iex(irm autopilotoobe.ps1.osdeploy.com)
+#>
+#=================================================
+#   Block WinPE
+#=================================================
+if ($env:SystemDrive -eq 'X:')
+{
+    Write-Warning 'PSCloudScript cannot be run from WinPE'
+    Start-Sleep -Seconds 5
+    break
 }
-Start-OSDCloud @Params
-#================================================
-#   WinPE PostOS Sample
-#   AutopilotOOBE Offline Staging
-#================================================
+#=================================================
+#   Require OOBE
+#=================================================
+if ($env:UserName -ne 'defaultuser0')
+{
+    Write-Warning 'PSCloudScript must be run from OOBE'
+    Start-Sleep -Seconds 5
+    break
+}
+#=================================================
+#	Set-ExecutionPolicy
+#=================================================
+if ((Get-ExecutionPolicy) -ne 'RemoteSigned')
+{
+    Write-Host -ForegroundColor Cyan 'Set-ExecutionPolicy RemoteSigned'
+    Set-ExecutionPolicy RemoteSigned -Force
+}
+#=================================================
+#	PackageManagement,PowerShellGet
+#=================================================
+if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
+    Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
+}
+else {
+    Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
+    Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery
+
+    Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
+    Import-Module PackageManagement,PowerShellGet -Force
+}
+#=================================================
+#	Set-PSRepository PSGallery
+#=================================================
+$PSRepository = Get-PSRepository -Name PSGallery
+if ($PSRepository)
+{
+    if ($PSRepository.InstallationPolicy -ne 'Trusted')
+    {
+        Write-Host -ForegroundColor Cyan 'Set-PSRepository PSGallery Trusted'
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
+}
+#=================================================
+#	WindowsAutopilotIntune
+#=================================================
+$Requirement = Import-Module WindowsAutopilotIntune -PassThru -ErrorAction Ignore
+if (-not $Requirement)
+{
+    Write-Host -ForegroundColor Cyan 'Install-Module AzureAD,Microsoft.Graph.Intune,WindowsAutopilotIntune'
+    Install-Module WindowsAutopilotIntune -Force
+}
+#=================================================
+#	AzureAD
+#=================================================
+$Requirement = Import-Module AzureAD -PassThru -ErrorAction Ignore
+if (-not $Requirement)
+{
+    Write-Host -ForegroundColor Cyan 'Install-Module AzureAD'
+    Install-Module AzureAD -Force
+}
+#=================================================
+#	Get-WindowsAutoPilotInfo
+#=================================================
+$Requirement = Get-InstalledScript -Name Get-WindowsAutoPilotInfod -ErrorAction SilentlyContinue
+if (-not $Requirement)
+{
+    Write-Host -ForegroundColor Cyan 'Install-Script Get-WindowsAutoPilotInfo'
+    Install-Script -Name Get-WindowsAutoPilotInfo -Force
+}
+#=================================================
+#	AutopilotOOBE
+#=================================================
+Write-Host -ForegroundColor Cyan 'Install-Module AutopilotOOBE'
 Install-Module AutopilotOOBE -Force
-Import-Module AutopilotOOBE -Force
-
-$Params = @{
-    Title = 'OSDeploy Autopilot Registration'
-    GroupTag = 'hsstudent'
-    GroupTagOptions = 'faculty','hsstudent'
-    Hidden = 'AddToGroup','AssignedComputerName','AssignedUser','PostAction'
-    Assign = $true
-    Run = 'NetworkingWireless'
-    Docs = 'https://autopilotoobe.osdeploy.com/'
-}
-AutopilotOOBE @Params
-#================================================
-#   WinPE PostOS Sample
-#   OOBEDeploy Offline Staging
-#================================================
-$Params = @{
-    Autopilot = $true
-    RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
-    UpdateDrivers = $true
-    UpdateWindows = $true
-}
-Start-OOBEDeploy @Params
-#================================================
-#   WinPE PostOS
-#   Set OOBEDeploy CMD.ps1
-#================================================
-$SetCommand = @'
-@echo off
-
-:: Set the PowerShell Execution Policy
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-
-:: Add PowerShell Scripts to the Path
-set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
-
-:: Open and Minimize a PowerShell instance just in case
-start PowerShell -NoL -W Mi
-
-:: Install the latest OSD Module
-start "Install-Module OSD" /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-
-:: Start-OOBEDeploy
-:: There are multiple example lines. Make sure only one is uncommented
-:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json
-start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy
-:: The next line assumes that you do not have a configuration saved in or want to ensure that these are applied
-REM start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows
-
-exit
-'@
-$SetCommand | Out-File -FilePath "C:\Windows\OOBEDeploy.cmd" -Encoding ascii -Force
-#================================================
-#   WinPE PostOS
-#   Set AutopilotOOBE CMD.ps1
-#================================================
-$SetCommand = @'
-@echo off
-
-:: Set the PowerShell Execution Policy
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-
-:: Add PowerShell Scripts to the Path
-set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
-
-:: Open and Minimize a PowerShell instance just in case
-start PowerShell -NoL -W Mi
-
-:: Install the latest AutopilotOOBE Module
-start "Install-Module AutopilotOOBE" /wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-
-:: Start-AutopilotOOBE
-:: There are multiple example lines. Make sure only one is uncommented
-:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json
-start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE
-:: The next line is how you would apply a CustomProfile
-REM start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -CustomProfile OSDeploy
-:: The next line is how you would configure everything from the command line
-REM start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -Title 'OSDeploy Autopilot Registration' -GroupTag Enterprise -GroupTagOptions Development,Enterprise -Assign
-
-exit
-'@
-$SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -Force
-#================================================
-#   PostOS
-#   Restart-Computer
-#================================================
-Restart-Computer
+#=================================================
+#	Complete
+#=================================================
+Write-Host -ForegroundColor Cyan 'Complete!'
+Write-Verbose 'Starting a new PowerShell process for Start-AutopilotOOBE' -Verbose
+Start-Sleep -Seconds 3
+Start-Process PowerShell.exe -ArgumentList '-NoLogo'
