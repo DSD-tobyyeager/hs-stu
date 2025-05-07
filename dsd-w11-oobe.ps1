f7410a69e87c722f13abc3904f1646a2a583bbed
DSD-tobyyeager <toby.yeager@doverschools.net>
2025-05-07 11:26:19 -0500

    Update dsd-w11-oobe.ps1

--- dsd-w11-oobe.ps1
+++ dsd-w11-oobe.ps1
@@ -108,7 +108,7 @@ $AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.Autopi
 # Create OOBE.cmd
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBETasks.CMD"
 $OOBECMD = @'
-start /Wait ms-settings:
+start /Wait cmd.exe /k ms-settings:
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Set-Timezone "Central Standard Time"
